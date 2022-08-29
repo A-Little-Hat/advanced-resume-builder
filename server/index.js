12b98c5b19271f9e708cd945f0df66cceb272ef7
subhamay2002 <subhamaynath2002@gmail.com>
2022-08-29 10:49:22 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express()
+const mongoose = require('mongoose');
+require('dotenv').config();
+const User = require('./Models/User')
+
+mongoose.connect(process.env.MONGO_URL);
+const database = mongoose.connection;
+database.on('error', (error) => {
+    console.log(error)
+})
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+
+
 app.listen(5000, ()=>{
     console.log("running on : localhost:5000");
 });
